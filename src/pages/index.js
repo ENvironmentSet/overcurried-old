aa4255099a9943bb61de2a0d7c1a0bd69f86a931
ENvironmentSet <sbshw1@gmail.com>
2019-07-26 20:07:20 +0900

    fix typo
    NOTE: IDK how gatsby build this until I found this typo. is gatsby uses magic?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import Bio from 'components/molecules/bio';
+import Bio from 'components/molecules/Bio';
 import BaseLayout from 'templates/BaseLayout';
 import SEO from 'templates/SEO';
 import HyperLink from 'components/molecules/HyperLink';
